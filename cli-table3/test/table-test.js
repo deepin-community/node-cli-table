1a46fd370bb6562d173e5cf466b8ff62c556c5c0
Tianyu Chen <sweetyfish@deepin.org>
2023-10-19 01:01:32 +0800

    feat: update node-cli-table to 0.3.11+_cs0.13.4

---
+++ cli-table3/test/table-test.js
@@ -0,0 +1,79 @@
+describe('@api Table ', function () {
+  const Table = require('..');
+  const colors = require('colors/safe');
+
+  it('wordWrap with colored text', function () {
+    let table = new Table({ style: { border: [], head: [] }, wordWrap: true, colWidths: [7, 9] });
+
+    table.push([colors.red('Hello how are you?'), colors.blue('I am fine thanks!')]);
+
+    let expected = [
+      '┌───────┬─────────┐',
+      '│ ' + colors.red('Hello') + ' │ ' + colors.blue('I am') + '    │',
+      '│ ' + colors.red('how') + '   │ ' + colors.blue('fine') + '    │',
+      '│ ' + colors.red('are') + '   │ ' + colors.blue('thanks!') + ' │',
+      '│ ' + colors.red('you?') + '  │         │',
+      '└───────┴─────────┘',
+    ];
+
+    expect(table.toString()).toEqual(expected.join('\n'));
+  });
+
+  it('allows numbers as `content` property of cells defined using object notation', function () {
+    let table = new Table({ style: { border: [], head: [] } });
+
+    table.push([{ content: 12 }]);
+
+    let expected = ['┌────┐', '│ 12 │', '└────┘'];
+
+    expect(table.toString()).toEqual(expected.join('\n'));
+  });
+
+  it('throws if content is not a string or number', function () {
+    let table = new Table({ style: { border: [], head: [] } });
+
+    expect(function () {
+      table.push([{ content: { a: 'b' } }]);
+      table.toString();
+    }).toThrow();
+  });
+
+  it('works with CJK values', function () {
+    let table = new Table({ style: { border: [], head: [] }, colWidths: [5, 10, 5] });
+
+    table.push(
+      ['foobar', 'English test', 'baz'],
+      ['foobar', '中文测试', 'baz'],
+      ['foobar', '日本語テスト', 'baz'],
+      ['foobar', '한국어테스트', 'baz']
+    );
+
+    let expected = [
+      '┌─────┬──────────┬─────┐',
+      '│ fo… │ English… │ baz │',
+      '├─────┼──────────┼─────┤',
+      '│ fo… │ 中文测试 │ baz │',
+      '├─────┼──────────┼─────┤',
+      '│ fo… │ 日本語…  │ baz │',
+      '├─────┼──────────┼─────┤',
+      '│ fo… │ 한국어…  │ baz │',
+      '└─────┴──────────┴─────┘',
+    ];
+
+    expect(table.toString()).toEqual(expected.join('\n'));
+  });
+});
+
+/*
+
+ let expected = [
+   '┌──┬───┬──┬──┐'
+ , '│  │   │  │  │'
+ , '├──┼───┼──┼──┤'
+ , '│  │ … │  │  │'
+ , '├──┼───┼──┼──┤'
+ , '│  │ … │  │  │'
+ , '└──┴───┴──┴──┘'
+ ];
+
+ */
---
+++ types-cli-table2/README.md
@@ -0,0 +1,16 @@
+# Installation
+> `npm install --save @types/cli-table2`
+
+# Summary
+This package contains type definitions for cli-table2 (https://github.com/jamestalmage/cli-table2).
+
+# Details
+Files were exported from https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/types/cli-table2.
+
+### Additional Details
+ * Last updated: Thu, 08 Jul 2021 09:07:56 GMT
+ * Dependencies: none
+ * Global values: none
+
+# Credits
+These definitions were written by [Melvin Groenhoff](https://github.com/mgroenhoff).
